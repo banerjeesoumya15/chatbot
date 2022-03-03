e09cf72d6ec0418d69b2a29fed44a831cdea7d9d
Soumya Banerjee <banerjeesoumya15@gmail.com>
2022-03-03 23:57:57 +0530

    Tokeniztion done

--- train_chatbot.py
+++ train_chatbot.py
@@ -3,7 +3,7 @@ from nltk.stem import WordNetLemmatizer
 import json
 import pickle
 
-import numpy as np
+#import numpy as np
 import random
 
 words = []
@@ -17,3 +17,16 @@ with open('intents.json') as data_file:
 
 # Data Preprocessing
 # tokenizing
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        # tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+
+        # add document to the corpus
+        documents.append((w, intent['tag']))
+
+        # add to classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
