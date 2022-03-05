9ab33757ee3266b03014c1da8d2cbc093c770d33
Soumya Banerjee <banerjeesoumya15@gmail.com>
2022-03-05 13:50:51 +0530

    baseline model created

--- train_chatbot.py
+++ train_chatbot.py
@@ -1,7 +1,12 @@
+from tabnanny import verbose
 import nltk
 from nltk.stem import WordNetLemmatizer
 import json
 import pickle
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
 
 import numpy as np
 import random
@@ -75,3 +80,21 @@ training = np.array(training, dtype=object)
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Train test data created")
+
+# Create model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# fitting and saving model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=10, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("Model saved")
